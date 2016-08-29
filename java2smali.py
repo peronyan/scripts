dae69145ca1efd086902737cc4058a500d4a09a1
peronyan <nakano@MacBookPro-nakano.local>
2016-08-29 14:52:16 +0900

    testcode

--- java2smali.py
+++ java2smali.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from google import search
 from multiprocessing import Process,Pool
 import sys
 import urllib,urllib2
-from bs4 import BeautifulSoup
-
 
 PROC = 12
 SEARCH_NUM = 20
 OUTPUT_PATH = "./output/"
 
-def googleSearch(word):
-	#outFileName = word.replace("/","-")+ ".csv"
-	#outFile = open(OUTPUT_PATH+outFileName,'w')
-	for url in search(word, stop = SEARCH_NUM,lang = "en"):
-		print url
-		#outFile.write(str(url))
-	#outFile.close()
-
 def main():
 	inputJavaCode = sys.argv[1]
+	javaCodes = []
+	for code in open(inputJavaCode):
+		code = code.strip()
+		if not code:
+			continue
+		javaCodes.append(code)
 
+	for code in javaCodes:
+		print code
 
 
 
