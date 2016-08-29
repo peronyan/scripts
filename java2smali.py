b0428eb6808e9cfae48058601fe2fdb82d7e7ac0
peronyan <nakano@MacBookPro-nakano.local>
2016-08-29 17:58:14 +0900

    ver1

--- java2smali.py
+++ java2smali.py
@@ -1,24 +1,89 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from multiprocessing import Process,Pool
-import sys
+import sys,re,os
 import urllib,urllib2
 
 PROC = 12
 SEARCH_NUM = 20
 OUTPUT_PATH = "./output/"
 
+def checkBracket(code):
+	global javaCodes
+	inBracketPattern = re.compile(r'[(].*?[)]')
+	intenBracketPattern = re.compile(r'[(].*[(].*[)].*[)]')
+	match1 = True
+	while(match1):
+		match1= inBracketPattern.search(code)
+		match2= intenBracketPattern.search(code)
+		if match2:
+			replaceText = match2.group()
+			text = replaceText[:-1]
+			text = text[1:]
+			argumentList = text.split(',')
+			for argument in argumentList:
+				#引数の中からメソッドでないものを消す
+				if argument.find("(") == -1:
+					continue
+				checkBracket(argument)
+			#code1 = checkBracket(text)
+			#print "code1:"+ code1
+			code = code.replace(replaceText,'')
+			match1 = False
+		if match1:
+			replaceText = match1.group()
+			code = code.replace(replaceText,'')
+				#print code
+		else:
+			match1 = False
+
+	javaCodes.append(code)
+
+
+
+
 def main():
 	inputJavaCode = sys.argv[1]
+	global javaCodes
 	javaCodes = []
+	methodPattern = re.compile(r'[(].*[)];')
 	for code in open(inputJavaCode):
 		code = code.strip()
 		if not code:
 			continue
-		javaCodes.append(code)
+		elif len(code) <= 3:
+			continue
+		matchedList = methodPattern.findall(code)
+		if len(matchedList) <= 0:
+			#print "no match"
+			continue
 
+		#=の一番後ろ側だけに絞る
+		code = code.split("=")[-1]
+		#super delete
+		#code = code.replace('super.','')
+		#code = code.replace('this.','')
+		#method name
+		checkBracket(code)
+
+
+
+	oFile = open('output.txt','w')
 	for code in javaCodes:
-		print code
+		code = code.strip()
+
+		code = code.replace(';','')
+		code = code.split('.')[-1]
+		code = code.strip("(")
+		code = code.strip(")")
+		if code.startswith("new"):
+			continue
+		if not code:
+			continue
+		oFile.write("->")
+		oFile.write(code)
+		oFile.write(os.linesep)
+	oFile.close()
 
 
 
