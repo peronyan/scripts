6120e32ddb2a060aba5d74dfa55a538fee1178ba
peronyan <nakano@MacBookPro-nakano.local>
2016-08-30 18:06:57 +0900

    08/30 my analysis

--- java2smali.py
+++ java2smali.py
@@ -9,13 +9,17 @@ SEARCH_NUM = 20
 OUTPUT_PATH = "./output/"
 
 def checkBracket(code):
+	global list_index
 	global javaCodes
+	methodPattern = re.compile(r'[(].*?[)];')
 	inBracketPattern = re.compile(r'[(].*?[)]')
-	intenBracketPattern = re.compile(r'[(].*[(].*[)].*[)]')
+	intenBracketPattern = re.compile(r'[(].*[(].*?[)].*?[)]')
 	match1 = True
+	#bracketFlag = False
 	while(match1):
 		match1= inBracketPattern.search(code)
 		match2= intenBracketPattern.search(code)
+		#match3 = methodPattern.search(code)
 		if match2:
 			replaceText = match2.group()
 
@@ -23,24 +27,30 @@ def checkBracket(code):
 			text = replaceText[:-1]
 			text = text[1:]
 			argumentList = text.split(',')
+			#print argumentList
 			for argument in argumentList:
-				argu = argument.split('.')
-				for arg in argu:
-					arg = arg.strip()
+				period = argument.split('.')
+				for p in period:
+					p = p.strip()
+					#print p
 				#引数の中からメソッドでないものを消す
-					if arg.find("(") == -1 and arg.find(")") == -1:
+					if not p.endswith(")") and not p.endswith(");"):
 						continue
-					checkBracket(arg)
+					checkBracket(p)
 			code = code.replace(replaceText,'')
 			#match1 = False
 		if match1:
+			#bracketFlag = True
 			replaceText = match1.group()
+			#print replaceText
+			#print replaceText
 			code = code.replace(replaceText,'')
 				#print code
 		else:
 			match1 = False
-
-	javaCodes.append(code)
+	#if bracketFlag:
+	appendText = code
+	javaCodes[list_index].append(appendText)
 
 
 
@@ -48,16 +58,41 @@ def checkBracket(code):
 def main():
 	inputJavaCode = sys.argv[1]
 	global javaCodes
-	javaCodes = []
+	global list_index
+	javaCodes = [[],[],[],[],[],[],[],[]]
+	list_index = 0
 	methodPattern = re.compile(r'[(].*[)];')
 	for code in open(inputJavaCode):
 		code = code.strip()
 		if not code:
 			continue
-		elif len(code) <= 3:
-			continue
+		#elif len(code) <= 3:
+		#	continue
+		if code.find("}") > -1:
+			#print code
+			list_index -= 1
+			#print list_index
+		if code.find("{") > -1:
+			#print code
+			list_index += 1
+			#print list_index
+				
 		matchedList = methodPattern.findall(code)
 		if len(matchedList) <= 0:
+			#if code.find("protected") <= -1 and code.find("public") <= -1:
+			if code.find("class") > -1:
+				if code.find("extends") > -1:
+					code = code.split("extends")[-1]
+					code = code.strip()
+					code = code.split(" ")[0]
+					if code.find("<") > -1:
+						code = code.split("<")[0]
+					className = "new " + code
+				else:
+					className = "new " + " Object"
+				print list_index
+				javaCodes[list_index].append(className)
+
 			#print "no match"
 			continue
 
@@ -67,25 +102,38 @@ def main():
 		#code = code.replace('super.','')
 		#code = code.replace('this.','')
 		#method name
+		print code
 		checkBracket(code)
 
-
-
+	writeList = []
 	oFile = open('output.txt','w')
-	for code in javaCodes:
-		code = code.strip()
-
-		code = code.replace(';','')
-		code = code.split('.')[-1]
-		code = code.strip("(")
-		code = code.strip(")")
-		if code.startswith("new"):
-			continue
-		if not code:
-			continue
-		oFile.write("->")
-		oFile.write(code)
-		oFile.write(os.linesep)
+	for code_list in javaCodes:
+		for code in code_list:
+			code = code.strip()
+			code = code.replace(';','')
+			code = code.split('.')[-1]
+			code = code.strip("(")
+			code = code.strip(")")
+			if not code:
+				continue
+			if len(code) <= 2:
+				continue
+			if code.find("{") > -1:
+				code = code.split(" ")[-2]
+			if code.startswith("new "):
+				code = code.lstrip("new ")
+				code = code + ";-><init>"
+			#continue
+			else:
+				code = "->" + code
+			oFile.write(code)
+			oFile.write(os.linesep)
+			#writeList.append(code)
+	
+	#for text in reversed(writeList):
+	#	oFile.write(text)
+	#	oFile.write(os.linesep)
+			#writeListoFile.write(os.linesep)
 	oFile.close()
 
 
---
+++ multi_smali_detectCode_v1.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import commands,sys,os,shlex,subprocess,re,datetime,locale
+from multiprocessing import Process,Pool
+import math
+
+proc = 12
+
+APK_DIR = "./apk/"
+CHECK_SMALI_DIR = "./check_java/"
+TEMP_DIR = "./temp/"
+OUTPUT_DIR = "./output/"
+SCORE_THRESHOLD = 70
+
+def findAllFiles(directory):
+    for root, dirs, files in os.walk(directory):
+		yield root
+		for file in files:
+			yield os.path.join(root, file)
+
+
+def deleteWhiteSpace(text):
+	text = text.strip()
+	return text
+
+def checkDeleteTargets(text):
+	dlist =[".local",".locals",".line"]
+	for dtext in dlist:
+		if dtext in text:
+			text = ""
+			break
+	return text
+
+def checkMultipleLines(smali,text):
+	tx = open(text,'r')
+	text_lines = tx.readlines()
+	tx.close()
+	text_index = 0
+	sl = open(smali,'r')
+	smali_lines = sl.readlines()
+	sl.close()
+	getData = []
+	for smali_line in smali_lines:
+		smali_line = deleteWhiteSpace(smali_line)
+		smali_line = checkDeleteTargets(smali_line)
+		#skip no text
+		if not smali_line:
+			continue
+		if deleteWhiteSpace(text_lines[text_index])in smali_line:
+			text_index +=1
+			if ".param" in smali_line:
+				#print smali_line
+				text = smali_line.strip()
+				text = text.replace(',','')
+				getData.append(text)
+			if text_index +1 >= len(text_lines):
+				getData.append(smali_lines[0].strip())
+				return True,getData
+		else:
+			text_index = 0
+			getData = []
+	return False,getData
+
+def checkSmali_Multi(apk,targetSmalis,codeSnippet):
+	global countNum
+	for smali in targetSmalis:
+		#for codeSnippet in codeSnippets:
+			#print "smali:"+ smali
+			#print "codeSnippet:" + codeSnippet
+			#getData =[]
+		flag,className,score = checkJava2SmaliCode(smali,codeSnippet)
+		if flag:
+			countNum += 1
+			writeOutput(apk,smali,className,codeSnippet,score,str(countNum))
+
+def checkJava2SmaliCode(smali,codeSnippet):
+	invokeList = []
+	className = ""
+	flag = True
+	for cSmali in open(smali):
+		if flag:
+			className = cSmali
+			flag = False
+		#print cSmali
+		if cSmali.find("invoke") > -1:
+			invokeList.append(cSmali)
+
+	score = calcScore(invokeList,codeSnippet)
+	if score >= SCORE_THRESHOLD:
+		#print className,str(score)
+		return True,className,score
+	else:
+		return False,className,score
+		
+def calcScore(invokeList,codeSnippet):
+	codes = []
+	for snippet in open(codeSnippet):
+		snippet = snippet.strip()
+		codes.append(snippet)
+	score = 0.0
+	doneFlag = []
+	addScore = float(100.0/len(codes))
+	firstFlag = True
+	firstIndex = -1
+	for index_c,code in enumerate(codes):
+		for index_m,invokeMethod in enumerate(invokeList):
+			if invokeMethod.find(code) > -1:
+				if firstFlag:
+					firstIndex = index_m
+					firstFlag = False
+				#print invokeMethod
+				if firstIndex > -1:#print code
+					denominator = (float)(math.fabs((index_m +1) - firstIndex))
+					#print denominator
+					if denominator < 1:
+						denominator = 1.0
+					score += (addScore/denominator)
+					firstIndex += 1
+					break
+	return score
+				#invokeMethod.pop(index_m)
+	
+
+def doApktool(apkPath):
+	print "apktool:" + apkPath
+	if not os.path.isdir(TEMP_DIR):
+		cmd = 'mkdir %s'%(TEMP_DIR)
+		subprocess.call(shlex.split(cmd))
+	workdir = TEMP_DIR + apkPath.split("/")[-1].rstrip(".apk")
+
+	cmd = 'apktool d -q -f %s -o %s'%(apkPath,workdir)
+	subprocess.call(shlex.split(cmd))
+
+def checkTrue(line):
+	line = line.split(",")[-1]
+	flag = line.find('0x1')
+	return flag
+
+def getCheckSmaliPath():
+	lists = []
+	for file in findAllFiles(CHECK_SMALI_DIR):
+		root,ext = os.path.splitext(str(file))
+		if ext == ".txt":
+			lists.append(str(file))
+	return lists
+
+def getAppSmaliPath(path):
+	lists = []
+	for file in findAllFiles(path):
+		root,ext = os.path.splitext(str(file))
+		if ext == ".smali":
+			lists.append(str(file))
+	return lists
+
+def getApkPath():
+	lists = []
+	for file in findAllFiles(APK_DIR):
+		root,ext = os.path.splitext(str(file))
+		if ext == ".apk":
+			lists.append(str(file))
+	return lists
+
+def deleteTmpFolder(path):
+	cmd = 'rm -r %s'%(path)
+	subprocess.call(shlex.split(cmd))
+
+def writeOutput(apk,smali,className,codeSnippet,score,countNum):
+	appName = apk.replace(APK_DIR,"")
+	global outfName
+	outPath = OUTPUT_DIR + outfName+ "-output.csv"
+	if not os.path.isdir(OUTPUT_DIR):
+		cmd = 'mkdir %s'%(OUTPUT_DIR)
+		subprocess.call(shlex.split(cmd))
+
+		
+	outPutFile = open(outPath,'a')
+	smali = smali.lstrip("./")
+	#codeSnippet = codeSnippet.replace(CHECK_SMALI_DIR,'')
+	codeSnippet = codeSnippet.strip()
+	className = className.strip()
+	text = appName +","+smali+","+className+","+codeSnippet+","+str(score)+","+countNum+os.linesep
+	#text = text.replace(";",os.linesep)
+	print text
+	outPutFile.write(text)
+	outPutFile.close()
+	
+def checkSmali(apk,targetSmalis,codeSnippets):
+	countNum = 0
+	for smali in targetSmalis:
+		for codeSnippet in codeSnippets:
+			checkSnippetNum = 0
+			totalSnippetNum = 0
+			for cCode in open(codeSnippet,'r'):
+				regflag = 0
+				totalSnippetNum += 1
+				reg = cCode.split(",")[0]
+				cCode = cCode.rstrip("¥n")
+				cCode = cCode.strip()
+				if reg == "regexp":
+					regflag = 1
+					cCode = cCode.lstrip("regexp,")
+					cCode = cCode.rstrip("¥n")
+					cCode = cCode.strip()
+					checkText = re.compile(cCode)
+				else:
+					cCode = cCode.rstrip("¥n")
+					cCode = cCode.strip()
+					checkText = cCode
+				line_list = []
+				for line in open(smali,'r'):
+					line_list.append(str(line))
+					
+				#with open(smali,'r') as smali_file:
+				for index,cSmali in enumerate(line_list):
+
+				#for cSmali in open(smali,'r'):
+					cSmali = cSmali.rstrip("¥n")
+					cSmali = cSmali.strip()
+					if regflag:
+						#print str(checkText)
+						flag = checkText.match(cSmali)
+					else:
+						if checkText in cSmali:
+							flag = 1
+						else:
+							flag = 0
+					if flag:
+						#print "aaaa"
+						trueFlag =checkTrue(str(line_list[index-2]))
+						if trueFlag:
+							checkSnippetNum += 1
+							#print "bbbb"
+						break
+			if checkSnippetNum >= totalSnippetNum:
+				countNum += 1
+				appName = apk.lstrip(APK_DIR)
+				writeOutput(apk,smali,codeSnippet,str(countNum))
+
+def doDecompile(apk,checksmali,index):
+	global countNum
+	countNum = 0
+	print 'process id:' + str(os.getpid())
+	#フォルダ名取得
+	root,ext = os.path.splitext(apk)
+	fName = root.split("/")[-1]
+	fName = TEMP_DIR + fName.rstrip(".apk") 
+	doApktool(apk)
+	smaliFName = str("./" + fName + "/smali/")
+	smaliPathLists = getAppSmaliPath(smaliFName)
+	checkSmali_Multi(apk,smaliPathLists,checksmali)
+	deleteTmpFolder(fName)
+	print "%s/%s"%(index+1,totalNum)
+
+	
+def argwrapper(args):
+	return args[0](*args[1:])
+
+if __name__ == "__main__":
+	global outfName
+	d = datetime.datetime.today()
+	outfName = d.strftime("%Y-%m-%d-%H:%M:%S")
+	doneNum = 0
+	apkLists = getApkPath()
+	print "APK Num:"+ str(len(apkLists))
+	global totalNum
+	totalNum = len(apkLists)
+	#checkLists = getCheckSmaliPath()	
+	#print "Check Num:"+ str(len(checkLists))
+	#デコンパイルループ
+	p = Pool()
+	func_args = []
+	for index,apk in enumerate(apkLists):
+		func_args.append((doDecompile,apk,sys.argv[1],index))
+	p.map(argwrapper,func_args)
+	
+
+		#argsList = []
+		#argsList.append(apk)
+		#argsList.append(checkLists)
+		#p.map(doDecompile,argsList)
+		#job = Process(target=doDecompile,args=(apk,checkLists))
+		#jobs.append(job)
+		#job.start()
+		#doneNum += 1
+		#print "%s/%s"%(doneNum,totalNum)
+
+
+	#[job.join() for job in jobs]
+	
+	#print "Finish!"
+
+	
